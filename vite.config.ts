97c5cb9ce7aef23d0feee42caebf72f9646122de
HsienLu <sjps946403@gmail.com>
2024-05-15 22:22:02 +0800

    修改config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/vite-react-ts/" : "/",
+  base:
+    process.env.NODE_ENV === "production" ? "/2024-hex-camp-mission2/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~': fileURLToPath(new URL('./node_modules', import.meta.url))
-    }
-  }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "~": fileURLToPath(new URL("./node_modules", import.meta.url)),
+    },
+  },
 });
