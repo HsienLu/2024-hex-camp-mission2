cf7a9ced1954c1ec22d8b160d27f70ecd64f56d5
HsienLu <hsienlu@mail.ntust.edu.tw>
2024-05-16 19:06:03 +0800

    section完成

--- src/component/HeroSection.tsx
+++ src/component/HeroSection.tsx
@@ -1,5 +1,17 @@
 import quoteLeft from "@/assets/image/quote-left.png";
 import quoteRight from "@/assets/image/quote-right.png";
+interface HeroSectionCardProps {
+  contentText: {
+    title: string;
+    text: string;
+  };
+}
+const contentText=[{
+  title:'2B經驗',
+  text:'內部監控系統、庫存系統、採購系統、行銷整合系統'
+},{  
+  title:'2C經驗',
+  text:'跨國美妝保養電商、個人品牌網站、醫美網站、電子商務'}]
 function HeroSection() {
   return (
     <>
@@ -21,7 +33,30 @@ function HeroSection() {
             <img src={quoteRight} alt="" />
           </span>
         </div>
+        <div className="container mt-4 pb-10">
+            <div className="row">
+              <div className="col-3 offset-3">
+
+              <HeroSectionCard contentText={contentText[0]}></HeroSectionCard>
+              </div>
+              <div className="col-3">
+
+              <HeroSectionCard contentText={contentText[1]}></HeroSectionCard>
+              </div>
+            </div>
+          </div>
       </div>
+    </>
+  );
+}
+function HeroSectionCard({ contentText }: HeroSectionCardProps) {
+  return (
+    <>
+    <div className="text-center">
+
+      <p className="fs-6 position-relative text-primary-700">{contentText.title}<span className="d-inline-block position-absolute positionMiddle bottom-0 border-bottom border-primary-400 border-2" style={{width:'24px'}}></span></p>
+      <p className="fs-6 text-primary-600">{contentText.text}</p>
+    </div>
     </>
   );
 }
